57db4e1a20f9dd667d7baa9691f805474244cd3a
Acatzu01 <laynoseanpatrick863@gmail.com>
2025-09-25 15:13:25 +0800

    Done

--- Attributes/AllowAnonymousSessionAttribute.cs
+++ Attributes/AllowAnonymousSessionAttribute.cs
@@ -2,9 +2,6 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApplication1.Attributes
 {
-    /// <summary>
-    /// Attribute to allow anonymous access and bypass session authorization
-    /// </summary>
     public class AllowAnonymousSessionAttribute : Attribute, IFilterMetadata
     {
     }
